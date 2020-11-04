7ffb8d1263841b414c5a4f1a0ab6dd6261373c90
lucaso1424 <lucaspadilla648@gmail.com>
2020-11-04 19:54:04 +0100

    lph - comitasBajas finales en segunda ventana

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // CREAMOS UN BOTON, COMO REFERENCIA DEL BOTON DE HTML
 let padre = null;
 let primero = null;
-var palabrasUsuario;
+var palabrasUsuario = [];
 var texto;
 var random;
 
@@ -13,19 +13,10 @@ function iniciar_juego() {
 }
 
 // INICIAMOS EL JUEGO CON WINDOW.ONLOAD LLAMANDO A LA FUNCION DE INICIO DEL JUEGO
-if(window.location.pathname == "/index.html"){
- window.onload = iniciar_juego();
- window.onload = abrir();
- console.log(window.location.pathname);
-} 
-
-// FUNCION ADD EVENT LISTENER QUE CUANDO EL BOTÓN RECIBA UN CLICK, POR ESO GENERAMOS EL LET
-
-
-// INICIAMOS EL JUEGO CON WINDOW.ONLOAD LLAMANDO A LA FUNCION DE INICIO DEL JUEGO
-
-// FUNCION ADD EVENT LISTENER QUE CUANDO EL BOTÓN RECIBA UN CLICK, POR ESO GENERAMOS EL LET
-
+if (window.location.pathname == "/index.html") {
+    window.onload = iniciar_juego();
+    window.onload = abrir();
+}
 
 // ABRIR VENTANAS EMERGENTES DE DIFERENTES ARCHIVOS DE PÁGINA, DONDE CREAMOS ESO EN UNA FUNCION
 
@@ -47,9 +38,14 @@ function abandonar_partida() {
 // FUNCION ADD EVENT LISTENER QUE CUANDO EL BOTÓN RECIBA UN CLICK, POR ESO GENERAMOS EL LET 
 
 function foto() {
-    for (var i = 0; i < 7; i++) {
-        document.getElementById("img0").src = '/img/Foto' + i + '.png';
-    }
+    document.write("<img id='img0' src='/img/Foto0.png' width='200'height='200'>");
+    /*for (var i = 0; i < 7; i++) {
+        document.write("<img id='img0' src='/img/Foto0.png' width='200' height='200'>");
+    }*/
+}
+
+if (window.location.pathname == "/ventanas/primera_ventana.html") {
+    window.onload = foto();
 }
 
 function Array() {
@@ -86,36 +82,28 @@ function eliminarCaracteres() {
         'Ü': "U"
 
     };
-
-    //acentos_espacios = acentos_espacios.replace(/Á/g,"")
-
     console.log(texto);
 }
 
-function borrarEspacios(){
+function borrarEspacios() {
     texto = texto.trim();
     //alert(texto);
     //for (var i=0; i<texto.length;i++){
     //if(texto.charAt(i) == " "){
 }
 
-// CREAMOS 
-
-function comitasBajas(){
+function comitasBajas() {
     let mostrar = [];
     texto = texto.split("")
-    
-    for(let letra of texto){
+
+    for (let letra of texto) {
         mostrar.push('_');
     }
 
-    document.write("<h1>"+mostrar+"</h1>");
+    document.write("<h1>" + mostrar + "</h1>");
 }
 
-if(window.location.pathname == "/ventanas/segunda_ventana.html") {
-    window.onload = comitasBajas();
 
-} 
 
 Array();
 getRandom();
